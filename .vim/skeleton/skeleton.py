446cff2f9c86c5643b1dc348cb413819b84c4716
Martin Bukatovic <martin.bukatovic@gmail.com>
2017-08-05 16:26:01 +0200

    vim: update python skeleton file

--- .vim/skeleton/skeleton.py
+++ .vim/skeleton/skeleton.py
@@ -1,14 +1,17 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python3
 # -*- coding: utf8 -*-
 
 
+import argparse
 import sys
 
 
-def main(argv=None):
-    argv = argv or sys.argv
-    if len(argv) <= 1:
-        return
+def main():
+    ap = argparse.ArgumentParser(description="")
+    ap.add_argument("somefile", nargs='?', help="...")
+    ap.add_argument("-d", "--dry-run", action="store_true", help="dry run")
+    args = ap.parse_args()
+
 
 if __name__ == '__main__':
     sys.exit(main())
