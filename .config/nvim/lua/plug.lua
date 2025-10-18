6c0c8f36c4faf500a81a920a2a40d3e187f7580d
Martin Bukatovic <martin.bukatovic@gmail.com>
2025-10-18 15:29:26 +0200

    neovim: orgmode plugin config

--- .config/nvim/lua/plug.lua
+++ .config/nvim/lua/plug.lua
@@ -1,3 +1,11 @@
 --
 -- plug.lua
 --
+
+-- https://github.com/nvim-orgmode/orgmode
+require('orgmode').setup({
+   org_agenda_files = '~/orgfiles/**/*',
+   org_default_notes_file = '~/orgfiles/refile.org',
+   org_adapt_indentation = false,
+   org_cycle_separator_lines = 0,
+})
