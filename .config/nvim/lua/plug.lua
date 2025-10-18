a45af4b3de01364b5c34ffb5326efa9c4d3e0ba5
Martin Bukatovic <martin.bukatovic@gmail.com>
2025-10-18 15:29:49 +0200

    neovim: gitsigns plugin config

--- .config/nvim/lua/plug.lua
+++ .config/nvim/lua/plug.lua
@@ -9,3 +9,18 @@ require('orgmode').setup({
    org_adapt_indentation = false,
    org_cycle_separator_lines = 0,
 })
+
+-- https://github.com/lewis6991/gitsigns.nvim
+require('gitsigns').setup({
+  signcolumn = true,   -- Toggle with `:Gitsigns toggle_signs`
+  numhl      = true,   -- Toggle with `:Gitsigns toggle_numhl`
+  linehl     = false,  -- Toggle with `:Gitsigns toggle_linehl`
+  word_diff  = false,  -- Toggle with `:Gitsigns toggle_word_diff`
+  watch_gitdir = {
+    follow_files = true
+  },
+  auto_attach = true,
+  max_file_length = 50000,
+  signs_staged_enable = true,
+  attach_to_untracked = false,
+})
