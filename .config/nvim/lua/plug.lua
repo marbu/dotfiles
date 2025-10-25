85978fc1de5432d9a37f638ab5edf8da0fe6b409
Martin Bukatovic <martin.bukatovic@gmail.com>
2025-10-26 00:30:14 +0200

    neovim: nvim-lastplace plugin config

--- .config/nvim/lua/plug.lua
+++ .config/nvim/lua/plug.lua
@@ -34,3 +34,10 @@ require('ayu').setup({
   },
 })
 require('ayu').colorscheme()
+
+-- https://github.com/ethanholz/nvim-lastplace
+require('nvim-lastplace').setup({
+  lastplace_ignore_buftype = {"quickfix", "nofile", "help"},
+  lastplace_ignore_filetype = {"gitcommit", "gitrebase", "svn", "hgcommit"},
+  lastplace_open_folds = true
+})
